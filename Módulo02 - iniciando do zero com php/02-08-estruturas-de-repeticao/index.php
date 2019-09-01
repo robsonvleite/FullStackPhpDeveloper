bc9bfde0372ed305610f8c67a20c9396c9f924d0
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 18:16:02 -0300

    Módulo02 - Estruturas de repetição (WHILE, DO WHILE)

--- Módulo02 - iniciando do zero com php/02-08-estruturas-de-repeticao/index.php
+++ Módulo02 - iniciando do zero com php/02-08-estruturas-de-repeticao/index.php
@@ -8,6 +8,26 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  */
 fullStackPHPClassSession("while, do while", __LINE__);
 
+    $looping = 1;
+    $while = [];
+
+    while ($looping <= 5) {
+        $while[] = $looping;
+        $looping++;
+    }
+    
+    var_dump($while);
+
+
+    $looping = 5;
+    $while = [];
+
+    do {
+        $while[] = $looping;
+        $looping--;
+    } while ($looping >= 1);
+
+    var_dump($while);
 
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
