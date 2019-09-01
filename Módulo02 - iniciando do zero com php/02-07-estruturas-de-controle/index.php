1929159dec7f87d07c1d5ac37d534ae98e3ef17c
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 15:36:17 -0300

    Módulo02 - Estruturas de Controle (ISSET, EMPTY)

--- Módulo02 - iniciando do zero com php/02-07-estruturas-de-controle/index.php
+++ Módulo02 - iniciando do zero com php/02-07-estruturas-de-controle/index.php
@@ -45,6 +45,23 @@ fullStackPHPClassSession("if, elseif, else", __LINE__);
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
+    $rock = "";
+
+    // isset — Informa se a variável foi iniciada
+    if (isset($rock)) {
+        var_dump("Rock And Roll!");
+    } else {
+        var_dump("Die");
+    }
+
+    $rockAndRoll = "Nirvana";
+
+    // empty — Determina se a variável é vazia
+    if (!empty($rockAndRoll)) {
+        var_dump("Rock existe e toca {$rockAndRoll}");
+    } else {
+        var_dump("Não exite ou não está tocando!");
+    }
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
