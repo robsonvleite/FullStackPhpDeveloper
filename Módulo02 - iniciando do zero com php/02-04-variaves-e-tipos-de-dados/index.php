240ee8827a2318e187d8d9d6497e405a076f7e4a
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 15:08:48 -0300

    Módulo02 - Variáveis e Tipos de Dados (VARIÁVEIS)

--- Módulo02 - iniciando do zero com php/02-04-variaves-e-tipos-de-dados/index.php
+++ Módulo02 - iniciando do zero com php/02-04-variaves-e-tipos-de-dados/index.php
@@ -8,6 +8,36 @@ fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
  */
 fullStackPHPClassSession("variáveis", __LINE__);
 
+    $userFirstName = "Robson";
+    $userLastName = "Leite";
+    echo "<h3>{$userFirstName} {$userLastName}</h3>";
+
+    $user_first_name = $userFirstName;
+    $user_last_name = $userLastName;
+    echo "<h3>{$user_first_name} {$user_last_name}</h3>";
+
+    $userAge = "32";
+    echo "<p>{$userFirstName} {$userLastName} <span class='tag'>tem {$userAge}</span></p>";
+
+    $userEmail = "<p>cursos@upinside.com.br</p>";
+    echo $userEmail;
+
+    //variável variável
+    $company = "UpInside";
+    $$company = "Treinamentos";
+    echo "<h3>{$company} {$UpInside}</h3>";
+
+
+    $calcA = 10;
+    $calcB = 20;
+    //$calcB = $calcA;
+    $calcB = &$calcA;
+    $calcB = 50;
+
+    var_dump([
+        "a" => $calcA,
+        "b" => $calcB
+    ]);
 
 /**
  * [ tipo boleano ] true | false
