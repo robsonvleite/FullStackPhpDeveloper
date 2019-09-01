b8768694cfedc51f2fa3cd175324d68cf637994e
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 14:48:07 -0300

    Iniciando curso Full Stack Php Developer

---
+++ iniciando-um-projeto/index.php
@@ -0,0 +1,2 @@
+<!DOCTYPE html><html lang="pt-br"><head>    <meta charset="UTF-8">
+        <meta name="mit" content="2019-09-01T14:44:55-03:00+167253">    <meta name="viewport" content="width=device-width,initial-scale=1">    <title>FSPHP: Iniciando Um Projeto!</title>    <link rel="stylesheet" href="assets/style.css"/></head><body><?php/** * Bloco PHP com variável start * e comando de saída! */$start = "Vamos Começar!";echo "<h1>Olá Mundo! {$start}</h1>";echo "<p id='js'>Loading...</p>";?><script src="assets/scripts.js"></script></body></html>
