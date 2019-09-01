ede9c6619bc6c2946b0a7bf98d1412716b7e40dd
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 15:32:03 -0300

    Módulo02 - Estruturas de Controle (IF, ELSEIF, ELSE)

--- Módulo02 - iniciando do zero com php/02-07-estruturas-de-controle/index.php
+++ Módulo02 - iniciando do zero com php/02-07-estruturas-de-controle/index.php
@@ -9,6 +9,35 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+    $test = true;
+
+    if ($test) {
+        var_dump(true);
+    } else {
+        var_dump(false);
+    }
+
+    $age = 51;
+
+    if ($age < 20) {
+        var_dump("Bandas Coloridas");
+    } elseif ($age > 20 && $age < 50) {
+        var_dump("Ótimas Bandas");
+    } else {
+        var_dump("Rock And Roll Raiz");
+    }
+
+    $hour = 3;
+
+    if ($hour >= 5 && $hour < 23) {
+        if ($hour < 7) {
+            var_dump("Bob Marley");
+        } else {
+            var_dump("After Bridge");
+        }
+    } else {
+        var_dump("ZzzzzzZZZZ");
+    }
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
