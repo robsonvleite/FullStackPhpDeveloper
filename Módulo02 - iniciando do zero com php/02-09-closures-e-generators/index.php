884996798b8151df22f8210ed26bf74068ea981d
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 18:24:41 -0300

    Módulo02 - Closures e Generators (CLOSURES)

--- Módulo02 - iniciando do zero com php/02-09-closures-e-generators/index.php
+++ Módulo02 - iniciando do zero com php/02-09-closures-e-generators/index.php
@@ -7,6 +7,30 @@ fullStackPHPClassName("02.09 - Closures e generators");
  */
 fullStackPHPClassSession("closures", __LINE__);
 
+    $myAge = function ($year) {
+        $age = date("Y") - $year;
+        return "<h5>Você tem {$age} anos!</h5>";
+    };
+
+    echo $myAge(1986);
+
+    $priceBrl = function ($price) {
+        return number_format($price, 2, ",", ".");
+    };
+
+    echo "<p>O projeto custa R$ {$priceBrl(3600)}. Vamos fechar?";
+
+    $myCart = [];
+    $myCart["totalPrice"] = 0;
+    $cardAdd = function ($item, $qtd, $price) use (&$myCart) {
+        $myCart[$item] = $qtd * $price;
+        $myCart["totalPrice"] += $myCart[$item];
+    };
+
+    $cardAdd("HTML5", 1, 497);
+    $cardAdd("jQuery", 2, 497);
+
+    var_dump($myCart);
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
