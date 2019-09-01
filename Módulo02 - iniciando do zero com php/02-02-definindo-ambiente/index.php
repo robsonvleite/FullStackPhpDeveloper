036aae1a46547c776ae1f65585bad416056e0365
Ricardo <ricardo.baldrez@gmail.com>
2019-09-01 14:54:46 -0300

    Módulo02 - Definindo Ambiente

---
+++ Módulo02 - iniciando do zero com php/02-02-definindo-ambiente/index.php
@@ -0,0 +1,11 @@
+<?php
+    require __DIR__ . '/../../fullstackphp/fsphp.php';
+    
+    fullStackPHPClassName("02.02 - Definindo ambiente");
+
+    /*
+    * Documentando o exemplo
+    */
+    fullStackPHPClassSession("Debug Section", __LINE__);
+
+    var_dump($_SERVER);
