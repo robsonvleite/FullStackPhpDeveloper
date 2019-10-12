563ff9bfec1f6a389f72c4b0539f636d49aaa107
Ricardo <ricardo.baldrez@gmail.com>
2019-10-12 18:53:00 -0300

    Módulo02 - Constantes e constantes mágicas(constantes mágicas), criação do arquivo MyClass.php

---
+++ Módulo02 - iniciando do zero com php/02-12-valores-constantes/MyClass.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robsonvleite
+ * Date: 07/08/2018
+ * Time: 20:06
+ */
+
+namespace Source;
+
+class MyClass
+{
+    public $namespace = __NAMESPACE__;
+}
--- Módulo02 - iniciando do zero com php/02-12-valores-constantes/index.php
+++ Módulo02 - iniciando do zero com php/02-12-valores-constantes/index.php
@@ -39,3 +39,36 @@ var_dump(get_defined_constants(true)["user"]);
  * [ constantes mágicas ] https://php.net/manual/pt_BR/language.constants.predefined.php
  */
 fullStackPHPClassSession("constantes mágicas", __LINE__);
+
+var_dump([
+    __LINE__,
+    __FILE__,
+    __DIR__
+]);
+
+function fullStackPHP()
+{
+    return __FUNCTION__;
+}
+
+var_dump(fullStackPHP());
+
+
+trait MyTrait
+{
+    public $traitName = __TRAIT__;
+}
+
+
+class FsPHP
+{
+    use MyTrait;
+    public $className = __CLASS__;
+}
+
+var_dump(new FsPHP());
+
+
+require __DIR__ . "/MyClass.php";
+var_dump(new \Source\MyClass());
+var_dump(\Source\MyClass::class);
