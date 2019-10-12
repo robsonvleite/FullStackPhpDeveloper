41bc17f92b043700d9170353891c72b465e6d608
Ricardo <ricardo.baldrez@gmail.com>
2019-10-12 18:38:33 -0300

    Módulo02 - TConstantes e constantes mágicasinamic arguments)

--- Módulo02 - iniciando do zero com php/02-12-valores-constantes/index.php
+++ Módulo02 - iniciando do zero com php/02-12-valores-constantes/index.php
@@ -7,6 +7,33 @@ fullStackPHPClassName("02.12 - Constantes e constantes mágicas");
  */
 fullStackPHPClassSession("constantes", __LINE__);
 
+define("COURSE", "Full Stack PHP");
+const AUTHOR = "Robson";
+
+$formation = true;
+if ($formation) {
+    define("COURSE_TYPE", "Formação");
+} else {
+    define("COURSE_TYPE", "Curso");
+}
+
+echo "<p>COURSE_TYPE COURSE AUTHOR</p>";
+echo "<p>{COURSE_TYPE} {COURSE} {AUTHOR}</p>";
+echo "<p>", COURSE_TYPE, " ", COURSE, " de ", AUTHOR, "</p>";
+echo "<p>" . COURSE_TYPE . " " . COURSE, " de " . AUTHOR . "</p>";
+
+
+class Config
+{
+    const USER = "root";
+    const HOST = "localhost";
+}
+
+echo "<p>", Config::HOST, "</p>";
+echo "<p>", Config::USER, "</p>";
+
+// Pegando as contantes definidas pelo usuário
+var_dump(get_defined_constants(true)["user"]);
 
 /*
  * [ constantes mágicas ] https://php.net/manual/pt_BR/language.constants.predefined.php
