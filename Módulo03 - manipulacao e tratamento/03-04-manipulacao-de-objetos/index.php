05d43e841c1561740dbcb0798767649fbede0f11
Ricardo <ricardo.baldrez@gmail.com>
2020-04-24 11:12:15 -0300

    Módulo03 - Manipulação de objetos(manipulação)

--- Módulo03 - manipulacao e tratamento/03-04-manipulacao-de-objetos/index.php
+++ Módulo03 - manipulacao e tratamento/03-04-manipulacao-de-objetos/index.php
@@ -7,8 +7,38 @@ fullStackPHPClassName("03.04 - Manipulação de objetos");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$arrProfile = [
+    "name" => "Robson",
+    "company" => "UpInside",
+    "mail" => "cursos@upinside.com.br"
+];
+
+// Criando um objeto a partir de uma array
+$objProfile = (object)$arrProfile;
+// var_dump($arrProfile, $objProfile);
+
+// Acessando através da array
+echo "<p>{$arrProfile['name']} trabalha na {$arrProfile['company']}</p>";
+// Acessando através do objeto 
+echo "<p>{$objProfile->name} trabalha na {$objProfile->company}</p>";
+
+$ceo = $objProfile;
+unset($ceo->company);
+var_dump($ceo);
+
+// Instanciando um objeto StdClass
+$company = new stdClass();
+$company->company = "UpInside";
+$company->ceo = $ceo;
+$company->manager = new stdClass();
+$company->manager->name = "Kaue";
+$company->manager->mail = "cursos@upinside.com.br";
+
+var_dump($company);
+
 
 /**
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
