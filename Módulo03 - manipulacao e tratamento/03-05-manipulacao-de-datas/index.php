ab7e630786bb62a0d783132c882392393e998fee
Ricardo <ricardo.baldrez@gmail.com>
2020-05-07 10:40:51 -0300

    Módulo03 - Manipulação de Datas(a função date)

--- Módulo03 - manipulacao e tratamento/03-05-manipulacao-de-datas/index.php
+++ Módulo03 - manipulacao e tratamento/03-05-manipulacao-de-datas/index.php
@@ -9,6 +9,28 @@ fullStackPHPClassName("03.05 - Manipulação de datas");
  */
 fullStackPHPClassSession("a função date", __LINE__);
 
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_W3C),
+    date("d/m/Y H:i:s")
+]);
+
+define("DATE_BR", "d/m/Y H:i:s");
+// define("DATE_TIMEZONE", "Pacific/Apia");
+define("DATE_TIMEZONE", "America/Sao_Paulo");
+
+date_default_timezone_set(DATE_TIMEZONE);
+
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_BR),
+    date(DATE_W3C),
+]);
+var_dump([
+    getdate(),
+]);
+
+echo "<p>Hoje é dia ", getdate()['mday'] , "</p>";
 
 /**
  * [ string to date ] strtotime | strftime
