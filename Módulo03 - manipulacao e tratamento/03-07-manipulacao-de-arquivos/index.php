6aa4bdeb47ec30bbeab7931d01ee95f8d6cb624e
Ricardo <ricardo.baldrez@gmail.com>
2020-05-12 11:34:35 -0300

    Módulo03 - Manipulação de arquivos(leitura e gravação)

--- Módulo03 - manipulacao e tratamento/03-07-manipulacao-de-arquivos/index.php
+++ Módulo03 - manipulacao e tratamento/03-07-manipulacao-de-arquivos/index.php
@@ -20,6 +20,26 @@ if(file_exists($file) && is_file($file)) { // Verificando se o arquivo existe e 
  */
 fullStackPHPClassSession("leitura e gravação", __LINE__);
 
+if(!file_exists($file) || !is_file($file)) {
+    $fileOpen = fopen($file, "w"); // Abri o arquivo(fopen) e põe em modo de leitura e gravação
+    fwrite($fileOpen, "Linha 01" . PHP_EOL); // Escrevendo no arquivo $fileOpen e concatenando com a quebra de linha (PHP_EOL)
+    fwrite($fileOpen, "Linha 02" . PHP_EOL);
+    fwrite($fileOpen, "Linha 03" . PHP_EOL);
+    fwrite($fileOpen, "Lorem Ipsum is simply dummy text of the printing and typesetting industry." . PHP_EOL);
+    fclose($fileOpen); // E depois da abertura e gravação, vem o fechamento do arquivo
+} else {
+    var_dump([
+        file($file),
+        pathinfo($file),
+    ]);
+}
+
+echo "<p>" . file($file)[3] . "</p><br>";
+
+$open = fopen($file, "r"); // "r" = Somente leitura
+while(!feof($open)) { // feof = Enquanto não chegar ao final do arquivo continue
+ echo "<p>". fgets($open) ."</p>"; // Pegando os dados do arquivo
+} 
 
 /*
  * [ get, put content ] file_get_contents | file_put_contents
