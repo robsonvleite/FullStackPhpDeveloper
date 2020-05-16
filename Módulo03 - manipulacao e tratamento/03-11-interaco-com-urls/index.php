bef535f020270040b08c1b51a4f2d021561572e6
Ricardo <ricardo.baldrez@gmail.com>
2020-05-16 15:03:22 -0300

    Módulo03 - Interação com URLs(argumentos)

--- Módulo03 - manipulacao e tratamento/03-11-interaco-com-urls/index.php
+++ Módulo03 - manipulacao e tratamento/03-11-interaco-com-urls/index.php
@@ -7,6 +7,29 @@ fullStackPHPClassName("03.11 - Interação com URLs");
  */
 fullStackPHPClassSession("argumentos", __LINE__);
 
+echo "<h1><a href='index.php'>Clear ... </a></h1>";
+echo "<p><a href='index.php?arg=1&arg=2'>Args</a></p>"; // Exemplo de um link criando args
+
+$data = [
+    "name" => "Ricardo",
+    "company" => "Google",
+    "mail" => "ricardo.baldrez@gmail.com"
+];
+
+$arguments = http_build_query($data); // http_build_query = transformando array em argumentos válidos para url (Gera a string de consulta (query) em formato URL)
+echo "<p><a href='index.php?{$arguments}'>Args by Array</a></p>";
+
+var_dump([
+    "GET" => $_GET,
+    "data" => $data,
+    "arguments" => $arguments,
+]);
+
+$object = (object)$data; // Transformando a array em um objeto
+var_dump(
+    $object,
+    http_build_query($object) // Transformando o objeto em argumentos de url
+);
 
 /*
  * [ segurança ] get | strip | filters | validation
