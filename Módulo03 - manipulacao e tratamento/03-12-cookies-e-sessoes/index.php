8ae8ccc164c5b5ec500b7d70ceb0c4bab004fd26
Ricardo <ricardo.baldrez@gmail.com>
2020-05-18 12:16:35 -0300

    Módulo03 - Cookies e sessões(sessões)

--- Módulo03 - manipulacao e tratamento/03-12-cookies-e-sessoes/index.php
+++ Módulo03 - manipulacao e tratamento/03-12-cookies-e-sessoes/index.php
@@ -29,8 +29,8 @@ setcookie(
     http_build_query($user), // dados do cookie(login)
     $time, // tempo de expiração do cookie
     "/", // valendo pra todo dominio
-    "localhost" // qual dominio ele vale
-    // true // garantindo que ele somente acesse seu dominio com segurança
+    "localhost", // qual dominio ele vale
+    true // garantindo que ele somente acesse seu dominio com segurança
 );
 
 $login = filter_input(INPUT_COOKIE, "fslogin", FILTER_SANITIZE_STRIPPED);
@@ -41,8 +41,43 @@ if($login) {
     var_dump($user);
 }
 
-
 /*
  * [ sessões ] http://php.net/manual/pt_BR/ref.session.php
  */
 fullStackPHPClassSession("sessões", __LINE__);
+
+$folder = __DIR__ . "/ses";
+if(!file_exists($folder) || !is_dir($folder)) {
+    mkdir($folder, 0755);
+} else {
+    echo "<p class='trigger accept'>O dir já existe</p>";
+}
+
+session_save_path(__DIR__ . "/ses"); // Pasta que salvaremos nossa sessão
+session_name("FSPHPSESSID"); // Dando um nome a nossa sessão
+session_start([
+    "cookie_lifetime" => (60 * 60 * 24)
+]); // iniciando a sessão(A sessão tem que ser iniciada logo na abertura do arquivo)
+
+var_dump(
+    $_SESSION, 
+    [
+        "id" => session_id(),
+        "name" => session_name(),
+        "status" => session_status(),
+        "save_path" => session_save_path(),
+        "cookie" => session_get_cookie_params()
+    ]
+);
+
+// Criando a sessão (login & user)
+$_SESSION['login'] = (object)$user;
+$_SESSION['user'] = $user;
+
+// Eliminando uma sessão específica
+// unset($_SESSION['login']);
+
+// Eliminando todas as sessões
+session_destroy();
+
+var_dump($_SESSION);
