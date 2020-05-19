7bb47d2a0c7d9dcf888266b95b37d83d5246dfbe
Ricardo <ricardo.baldrez@gmail.com>
2020-05-19 19:28:34 -0300

    Módulo04 - Qualificação e encapsulamento(namespaces)

--- Módulo04 - php orientado a objetos/04-03-qualificacao-e-encapsulamento/index.php
+++ Módulo04 - php orientado a objetos/04-03-qualificacao-e-encapsulamento/index.php
@@ -7,7 +7,31 @@ fullStackPHPClassName("04.03 - Qualificação e encapsulamento");
  */
 fullStackPHPClassSession("namespaces", __LINE__);
 
+// 1º forma de utlizar o namespace
 
+require __DIR__ . "/classes/App/Template.php";
+require __DIR__ . "/classes/Web/Template.php";
+
+$appTemplate = new App\Template();
+$webTemplate = new Web\Template();
+
+var_dump(
+    $appTemplate,
+    $webTemplate
+);
+
+// 2º forma de utlizar o namespace
+
+use App\Template;
+use Web\Template AS WebTemplate; // Renomeando para que não haja confilto por ter o mesmo nome
+
+$appUseTemplate = new Template();
+$webUseTemplate = new WebTemplate();
+
+var_dump(
+    $appTemplate,
+    $webTemplate
+);
 /*
  * [ visibilidade ] http://php.net/manual/pt_BR/language.oop5.visibility.php
  */
