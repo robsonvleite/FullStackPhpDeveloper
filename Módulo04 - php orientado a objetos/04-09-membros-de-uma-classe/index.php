745a1c4a29eaa587a9b1a49dcb2c06b4257241a6
Ricardo <ricardo.baldrez@gmail.com>
2020-05-26 10:54:28 -0300

    Módulo04 - Membros de uma classe(constantes)

--- Módulo04 - php orientado a objetos/04-09-membros-de-uma-classe/index.php
+++ Módulo04 - php orientado a objetos/04-09-membros-de-uma-classe/index.php
@@ -9,6 +9,20 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("constantes", __LINE__);
 
+use Source\Members\Config;
+
+$config = new Config();
+echo "<p>" . $config::COMPANY . "</p>";
+
+var_dump(
+    Config::COMPANY
+    // Config::DOMAIN,
+    // Config::SECTOR
+);
+
+$reflection = new ReflectionClass(Config::class); // Traz uma classe pronta para debugar qualquer rotina de classe
+
+var_dump($config, $reflection, $reflection->getConstants(), get_class_methods($reflection));
 
 /*
  * [ propriedades ] http://php.net/manual/pt_BR/language.oop5.static.php
---
+++ Módulo04 - php orientado a objetos/04-09-membros-de-uma-classe/source/Members/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Source\Members;
+
+class Config
+{
+    public const COMPANY = "UpInside";
+    protected const DOMAIN = "upinside.com.br";
+    private const SECTOR = "Educação";
+}
