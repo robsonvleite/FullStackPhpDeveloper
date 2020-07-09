9098057203ca4a74fca241089911038789a49613
Ricardo <ricardo.baldrez@gmail.com>
2020-07-09 12:39:34 -0300

    Módulo 05 - PDOStatement e bind modes(stmt bind value)

--- Módulo05 - banco de dados com pdo/05-07-pdostatements-e-bind-modes/index.php
+++ Módulo05 - banco de dados com pdo/05-07-pdostatements-e-bind-modes/index.php
@@ -31,6 +31,47 @@ var_dump(
  */
 fullStackPHPClassSession("stmt bind value", __LINE__);
 
+// bind value -> É a substituição de posições na sua instrução SQL por valores tratados pelo PDO
+
+// $stmt = Connect::getInstance()->prepare(
+//     "INSERT INTO users (first_name, last_name, email, document) VALUES (?, ?, ?, ?)"
+// );
+
+// $stmt->bindValue(1, "Gustavo", PDO::PARAM_STR);
+// $stmt->bindValue(2, "Web", PDO::PARAM_STR);
+// $stmt->bindValue(3, "gustavo@hotmail.com", PDO::PARAM_STR);
+// $stmt->bindValue(4, "2", PDO::PARAM_INT);
+
+// $stmt->execute();
+
+// var_dump(
+//     $stmt,
+//     $stmt->rowCount(),
+//     Connect::getInstance()->lastInsertId()
+// );
+
+$nome = "Nathália";
+
+$stmt = Connect::getInstance()->prepare(
+    "INSERT INTO users (first_name, last_name, email, document) VALUES (:first_name, :last_name, :email, :document)"
+);
+
+$stmt->bindValue(":first_name", $nome, PDO::PARAM_STR);
+$stmt->bindValue(":last_name", "Oliveira", PDO::PARAM_STR);
+$stmt->bindValue(":email","nathália@gmail.com", PDO::PARAM_STR);
+$stmt->bindValue(":document","5", PDO::PARAM_INT);
+
+$stmt->execute();
+
+var_dump(
+    $stmt,
+    $stmt->rowCount(),
+    Connect::getInstance()->lastInsertId()
+);
+
+if(Connect::getInstance()->lastInsertId() > 50) {
+    Connect::getInstance()->exec("DELETE FROM users WHERE id > 50");
+}
 
 /*
  * [ bind param ] http://php.net/manual/pt_BR/pdostatement.bindparam.php
